1bf0997153aaf38e2650f1130abc4a56df9dcfd1
Samuel Mráz <samko.mraz@seznam.cz>
2022-06-14 21:40:05 +0200

    changes

--- Assets/Scripts/editMode.cs
+++ Assets/Scripts/editMode.cs
@@ -3,20 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System.Windows.Forms;
 
 public class editMode : MonoBehaviour
 {
-    public TextMeshProUGUI statusText;
-    public Image image;
 
-    public void Choose()
-    {
-        Debug.Log(Clipboard.GetText());
-    }
-
-    public void Render()
-    {
-
-    }
 }
